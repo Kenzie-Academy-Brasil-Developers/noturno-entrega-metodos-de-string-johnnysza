bf2465b4810d2e850d1087c7bf4f2d62db7535ca
johnnysza <johnsilvasza@gmail.com>
2022-01-05 21:44:44 -0300

    Métodos De Strings - Lógicas de botão

--- src/script.js
+++ src/script.js
@@ -1,1 +1,26 @@
 // Seu código vai aqui;
+
+function showResults (){
+    const textContent = document.getElementById("textContent");
+    textContent.innerHTML = ""
+    let texto = document.getElementById("stringInput").value;
+    const resultado = document.createElement("p");
+    resultado.innerHTML = texto
+
+    textContent.appendChild(resultado);
+}
+
+
+
+function teste(){
+    let texto = document.getElementById("stringInput").value;   
+    document.getElementById("textContent").innerHTML = texto;
+    
+}
+
+const botaoUpper =  document.getElementById("toUpperCase");
+botaoUpper.addEventListener("click", (event) => {
+    event.preventDefault();
+    showResults()
+})
+
